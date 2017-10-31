1bf565c0eb8f6c1c4c785320c91e5ca40e8215c9
Irmina Kaczmarek <irminakacz@gmail.com>
2017-10-31 21:07:21 +0100

    add first acceptance tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,48 @@
 import { AppPage } from './app.po';
+import { by, element, browser } from 'protractor';
 
 describe('frontend App', () => {
-  let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
+  it('should login', () => {
+    browser.get('/login');
+    element(by.id('username')).sendKeys('test');
+    element(by.id('password')).sendKeys('zaq1@WSX');
+    element(by.id('login-btn')).click();
+
+    expect(element(by.id('header')).getText()).toEqual('Decks');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should fail login - incorrect credentials', () => {
+    browser.get('/login');
+    element(by.id('username')).sendKeys('test_fail');
+    element(by.id('password')).sendKeys('zaq1@WSX');
+    element(by.id('login-btn')).click();
+
+    expect(element(by.id('error-message')).getText())
+      .toEqual("Incorrect username or password.");
+  });
+
+  it('should fail login - no credentials', () => {
+    browser.get('/login');
+    element(by.id('login-btn')).click();
+
+    expect(element(by.id('error-message')).getText())
+      .toEqual("Fields cannot be empty.");
+  });
+
+  it('should create new user and login', () => {
+    browser.get('/login');
+    element(by.id('goto-create-user-btn')).click();
+    element(by.id('username')).sendKeys('test_new');
+    element(by.id('password')).sendKeys('zaq1@WSX');
+    element(by.id('repeatedPassword')).sendKeys('zaq1@WSX');
+    element(by.id('create-user-btn')).click();
+    expect(element(by.id('create-user-success-message')).getText())
+      .toEqual("Account created successfuly.");
+    element(by.id('create-user-goback-btn')).click();
+    element(by.id('username')).sendKeys('test_new');
+    element(by.id('password')).sendKeys('zaq1@WSX');
+    element(by.id('login-btn')).click();
+    expect(element(by.id('header')).getText()).toEqual('Decks');
   });
 });
